5bbc08afac49426bbb86c2c4ef0f12d6fd8fa97f
evan <ejeffries14@gmail.com>
2021-11-07 02:36:21 -0600

    commit tree 3cd8567af59717625de97344d4a9b3d423d11951

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,21 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+    attr_reader :title, :content
+
+    @@all = []
+    def initialize(params)
+        @title = params[:title]
+        @content = params[:content]
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+
+    def self.all
+        @@all
+    end
 end
